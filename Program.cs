98e0b2b0b58afa489857177332a34d80ed79f35c
VladislavSCV <116951315+VladislavSCV@users.noreply.github.com>
2022-11-06 16:04:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿string nameof = "Vlad";
+Console.WriteLine("Привет, как тебя зовут?");
+nameof = Console.ReadLine();
+Console.WriteLine("Привет {0}", nameof);
+nameof = Console.ReadLine();
+
+Console.WriteLine("Хочешь по говорить со мной о чем нибудь?");
+nameof = Console.ReadLine();
+
+Console.WriteLine("Хватит ломаться давай поговорим.{0}", ("Ну пожалуйста"));
+nameof = Console.ReadLine();
+Console.WriteLine("Отлично.О чем поговорим?");
+nameof = Console.ReadLine();
+
+Console.WriteLine("Бллииииииннн,Как грустнооо.Я говорю только о программировании, но вскоре я научусь обсуждать все и всех.");
+nameof = Console.ReadLine();
+Console.WriteLine("Ой,меня мама зовет кушать. Прости,я тебя не смогу запомнить тебя но, с тобой было очень интересно общаться ");
+nameof = Console.ReadLine();
+Console.WriteLine("Прощай");
+
+
+
